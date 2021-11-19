261ca76bb7ec3e310ba2ea8b72f3288a3965c86a
BostonTLee <boston.lee@gmail.com>
2021-11-18 23:24:00 -0700

    Script for copying images generated by working document

---
+++ paper/copy-images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp ../working_document/maps/prop_alcohol_use_disorder_aggregated.png images/
+cp ../working_document/maps/prop_MDE_aggregated.png images/
+cp ../working_document/maps/prop_suicidal_thoughts_aggregated.png images/
+cp ../working_document/maps/prop_below_poverty_level_aggregated.png images/
+cp ../working_document/maps/*fitted.png images/
+cp ../working_document/maps/*resid*.png images/
